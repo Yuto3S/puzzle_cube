df894e110f46bb98309971336c2d22dc79b001a1
Hugues Marandeau <h.marandeau@gmail.com>
2023-03-22 20:13:39 +0100

    Add logic to generate translated pieces over x/y/z and use it to generate all 960 possibles pieces in a 5x5x5 cube

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use crate::piece::Piece;
+
 mod piece;
 
 fn main() {
@@ -11,21 +13,28 @@ fn main() {
         false,
         piece::are_pieces_overlapping(default_pieces[0], default_pieces[21])
     );
+
+    let mut all_generated_pieces: Vec<Piece> = vec![];
+
+    for default_piece in default_pieces.iter() {
+        all_generated_pieces.push(*default_piece);
+        all_generated_pieces.append(&mut piece::generate_all_pieces_from_translating(
+            *default_piece,
+        ));
+    }
+
+    assert_eq!(960, all_generated_pieces.len());
 }
 
 fn test_benchmark_given_function() -> () {
-    const VEC_LENGTH: usize = 100000;
+    const VEC_LENGTH: usize = 10000;
 
     benchmarking::warm_up();
 
     let bench_result = benchmarking::measure_function(|measurer| {
         let mut vec: Vec<usize> = Vec::with_capacity(VEC_LENGTH);
 
-        measurer.measure(|| {
-            for i in 0..VEC_LENGTH {
-                // WRITE CODE FOR GIVEN FUNCTION HERE
-            }
-        });
+        measurer.measure(|| for i in 0..VEC_LENGTH {});
 
         vec
     })
--- src/piece.rs
+++ src/piece.rs
@@ -5,13 +5,19 @@ pub struct Coordinates {
     pub z: u8,
 }
 
-#[derive(PartialEq, Debug, Copy, Clone)]
+#[derive(PartialEq, Ord, Eq, PartialOrd, Debug, Copy, Clone)]
 pub struct Piece {
     pub block_1: Coordinates,
     pub block_2: Coordinates,
     pub block_3: Coordinates,
     pub block_4: Coordinates,
     pub block_5: Coordinates,
+}
+
+pub enum TranslatePieceOver {
+    X,
+    Y,
+    Z,
 }
 
 impl Piece {
@@ -27,10 +33,28 @@ impl Piece {
             return false;
         }
     }
+
+    pub fn is_valid(&self) -> bool {
+        return self.block_1.x < 5
+            && self.block_1.y < 5
+            && self.block_1.z < 5
+            && self.block_2.x < 5
+            && self.block_2.y < 5
+            && self.block_2.z < 5
+            && self.block_3.x < 5
+            && self.block_3.y < 5
+            && self.block_3.z < 5
+            && self.block_4.x < 5
+            && self.block_4.y < 5
+            && self.block_4.z < 5
+            && self.block_5.x < 5
+            && self.block_5.y < 5
+            && self.block_5.z < 5;
+    }
 }
 
 // Those pieces/coordinates can be visually seen in https://github.com/Yuto3S/puzzle_cube/blob/master/src/assets/24_default.png
-const ALL_DEFAULT_PIECES_COORDINATES: [[(u8, u8, u8); 5] ; 24] = [
+const ALL_DEFAULT_PIECES_COORDINATES: [[(u8, u8, u8); 5]; 24] = [
     [(0, 0, 0), (1, 0, 0), (2, 0, 0), (2, 0, 1), (3, 0, 0)],
     [(0, 1, 0), (1, 1, 0), (2, 0, 0), (2, 1, 0), (3, 1, 0)],
     [(0, 0, 1), (1, 0, 1), (2, 0, 1), (2, 0, 0), (3, 0, 1)],
@@ -54,7 +78,7 @@ const ALL_DEFAULT_PIECES_COORDINATES: [[(u8, u8, u8); 5] ; 24] = [
     [(1, 0, 1), (0, 0, 0), (0, 0, 1), (0, 0, 2), (0, 0, 3)],
     [(0, 0, 1), (0, 1, 0), (0, 1, 1), (0, 1, 2), (0, 1, 3)],
     [(0, 0, 1), (1, 0, 0), (1, 0, 1), (1, 0, 2), (1, 0, 3)],
-    [(0, 1, 1), (0, 0, 0), (0, 0, 1), (0, 0, 2), (0, 0, 3)]
+    [(0, 1, 1), (0, 0, 0), (0, 0, 1), (0, 0, 2), (0, 0, 3)],
 ];
 
 pub fn get_all_default_pieces() -> [Piece; 24] {
@@ -105,4 +129,135 @@ pub fn are_pieces_overlapping(piece_1: Piece, piece_2: Piece) -> bool {
     }
 
     return false;
+}
+
+fn maybe_create_new_piece_by_translating_piece_over_(
+    piece: Piece,
+    translate_over: TranslatePieceOver,
+) -> Option<Piece> {
+    let mut x = 0;
+    let mut y = 0;
+    let mut z = 0;
+
+    match translate_over {
+        TranslatePieceOver::X => {
+            x = 1;
+        }
+        TranslatePieceOver::Y => {
+            y = 1;
+        }
+        TranslatePieceOver::Z => {
+            z = 1;
+        }
+    }
+
+    let new_piece = Piece {
+        block_1: Coordinates {
+            x: piece.block_1.x + x,
+            y: piece.block_1.y + y,
+            z: piece.block_1.z + z,
+        },
+        block_2: Coordinates {
+            x: piece.block_2.x + x,
+            y: piece.block_2.y + y,
+            z: piece.block_2.z + z,
+        },
+        block_3: Coordinates {
+            x: piece.block_3.x + x,
+            y: piece.block_3.y + y,
+            z: piece.block_3.z + z,
+        },
+        block_4: Coordinates {
+            x: piece.block_4.x + x,
+            y: piece.block_4.y + y,
+            z: piece.block_4.z + z,
+        },
+        block_5: Coordinates {
+            x: piece.block_5.x + x,
+            y: piece.block_5.y + y,
+            z: piece.block_5.z + z,
+        },
+    };
+
+    if new_piece.is_valid() {
+        return Some(new_piece);
+    }
+
+    return None;
+}
+
+fn maybe_create_new_piece_by_translating_piece_over_x(piece: Piece) -> Option<Piece> {
+    return maybe_create_new_piece_by_translating_piece_over_(piece, TranslatePieceOver::X);
+}
+
+fn maybe_create_new_piece_by_translating_piece_over_y(piece: Piece) -> Option<Piece> {
+    return maybe_create_new_piece_by_translating_piece_over_(piece, TranslatePieceOver::Y);
+}
+
+fn maybe_create_new_piece_by_translating_piece_over_z(piece: Piece) -> Option<Piece> {
+    return maybe_create_new_piece_by_translating_piece_over_(piece, TranslatePieceOver::Z);
+}
+
+fn create_all_new_pieces_by_translating_over_x_y_z(piece: Piece) -> Vec<Piece> {
+    let mut translations_over_x_y_z: Vec<Piece> = vec![];
+
+    let new_piece = maybe_create_new_piece_by_translating_piece_over_x(piece);
+
+    if new_piece.is_some() {
+        translations_over_x_y_z.push(new_piece.unwrap());
+        translations_over_x_y_z.extend(create_all_new_pieces_by_translating_over_x_y_z(
+            new_piece.unwrap(),
+        ));
+        translations_over_x_y_z.extend(create_all_new_pieces_by_translating_over_y_z(
+            new_piece.unwrap(),
+        ));
+        translations_over_x_y_z.extend(create_all_new_pieces_by_translating_over_z(
+            new_piece.unwrap(),
+        ));
+    }
+
+    return translations_over_x_y_z;
+}
+
+fn create_all_new_pieces_by_translating_over_y_z(piece: Piece) -> Vec<Piece> {
+    let mut translations_over_y_z: Vec<Piece> = vec![];
+
+    let new_piece = maybe_create_new_piece_by_translating_piece_over_y(piece);
+
+    if new_piece.is_some() {
+        translations_over_y_z.push(new_piece.unwrap());
+        translations_over_y_z.extend(create_all_new_pieces_by_translating_over_y_z(
+            new_piece.unwrap(),
+        ));
+        translations_over_y_z.extend(create_all_new_pieces_by_translating_over_z(
+            new_piece.unwrap(),
+        ));
+    }
+
+    return translations_over_y_z;
+}
+
+fn create_all_new_pieces_by_translating_over_z(piece: Piece) -> Vec<Piece> {
+    let mut translations_over_z: Vec<Piece> = vec![];
+
+    let new_piece = maybe_create_new_piece_by_translating_piece_over_z(piece);
+
+    if new_piece.is_some() {
+        translations_over_z.push(new_piece.unwrap());
+        translations_over_z.extend(create_all_new_pieces_by_translating_over_z(
+            new_piece.unwrap(),
+        ));
+    }
+
+    return translations_over_z;
+}
+
+pub fn generate_all_pieces_from_translating(piece: Piece) -> Vec<Piece> {
+    let mut all_pieces_by_translation: Vec<Piece> = vec![];
+
+    all_pieces_by_translation.extend(create_all_new_pieces_by_translating_over_x_y_z(piece));
+    all_pieces_by_translation.extend(create_all_new_pieces_by_translating_over_y_z(piece));
+    all_pieces_by_translation.extend(create_all_new_pieces_by_translating_over_z(piece));
+
+    return all_pieces_by_translation;
 }
