0bb1b57ecc3d53089efc5dabb58786be1b93bc89
Hugues Marandeau <h.marandeau@gmail.com>
2023-03-22 17:47:11 +0100

    Add benchmarking function, update Piece.contains & are_pieces_overlapping to be more efficient time wise

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,39 @@
 mod piece;
 
 fn main() {
-    println!("Hello, world!");
     let default_pieces = piece::get_all_default_pieces();
-    // for piece in default_pieces.iter(){
-    //     println!("{}", piece.block_1.x);
-    // }
 
-    assert_eq!(true, piece::are_pieces_overlapping(default_pieces[0], default_pieces[2]));
-    assert_eq!(false, piece::are_pieces_overlapping(default_pieces[0], default_pieces[21]));
+    assert_eq!(
+        true,
+        piece::are_pieces_overlapping(default_pieces[0], default_pieces[2])
+    );
+    assert_eq!(
+        false,
+        piece::are_pieces_overlapping(default_pieces[0], default_pieces[21])
+    );
 }
 
+fn test_benchmark_given_function() -> () {
+    const VEC_LENGTH: usize = 100000;
+
+    benchmarking::warm_up();
+
+    let bench_result = benchmarking::measure_function(|measurer| {
+        let mut vec: Vec<usize> = Vec::with_capacity(VEC_LENGTH);
+
+        measurer.measure(|| {
+            for i in 0..VEC_LENGTH {
+                // WRITE CODE FOR GIVEN FUNCTION HERE
+            }
+        });
+
+        vec
+    })
+    .unwrap();
+
+    println!(
+        "are_pieces_overlapping {} times takes {:?}!",
+        VEC_LENGTH,
+        bench_result.elapsed()
+    );
+}
--- src/piece.rs
+++ src/piece.rs
@@ -1,10 +1,8 @@
-use std::collections::HashSet;
-
-#[derive(Hash, Ord, Eq, PartialOrd, PartialEq, Debug, Copy, Clone)]
+#[derive(Hash, PartialEq, Ord, Eq, PartialOrd, Debug, Copy, Clone)]
 pub struct Coordinates {
-    pub x: i8,
-    pub y: i8,
-    pub z: i8,
+    pub x: u8,
+    pub y: u8,
+    pub z: u8,
 }
 
 #[derive(PartialEq, Debug, Copy, Clone)]
@@ -16,9 +14,23 @@ pub struct Piece {
     pub block_5: Coordinates,
 }
 
+impl Piece {
+    pub fn contains(&self, coordinate: &Coordinates) -> bool {
+        if &self.block_1 == coordinate
+            || &self.block_2 == coordinate
+            || &self.block_3 == coordinate
+            || &self.block_4 == coordinate
+            || &self.block_5 == coordinate
+        {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
 
 // Those pieces/coordinates can be visually seen in https://github.com/Yuto3S/puzzle_cube/blob/master/src/assets/24_default.png
-const ALL_DEFAULT_PIECES_COORDINATES: [[(i8, i8, i8); 5] ; 24] = [
+const ALL_DEFAULT_PIECES_COORDINATES: [[(u8, u8, u8); 5] ; 24] = [
     [(0, 0, 0), (1, 0, 0), (2, 0, 0), (2, 0, 1), (3, 0, 0)],
     [(0, 1, 0), (1, 1, 0), (2, 0, 0), (2, 1, 0), (3, 1, 0)],
     [(0, 0, 1), (1, 0, 1), (2, 0, 1), (2, 0, 0), (3, 0, 1)],
@@ -46,25 +58,51 @@ const ALL_DEFAULT_PIECES_COORDINATES: [[(i8, i8, i8); 5] ; 24] = [
 ];
 
 pub fn get_all_default_pieces() -> [Piece; 24] {
-    let default_pieces: [Piece; 24] = ALL_DEFAULT_PIECES_COORDINATES.iter().map(
-        |coordinates| 
-        Piece {
-            block_1: Coordinates { x: coordinates[0].0, y: coordinates[0].1, z: coordinates[0].2 },
-            block_2: Coordinates { x: coordinates[1].0, y: coordinates[1].1, z: coordinates[1].2 },
-            block_3: Coordinates { x: coordinates[2].0, y: coordinates[2].1, z: coordinates[2].2 },
-            block_4: Coordinates { x: coordinates[3].0, y: coordinates[3].1, z: coordinates[3].2 },
-            block_5: Coordinates { x: coordinates[4].0, y: coordinates[4].1, z: coordinates[4].2 }
-        }
-    ).collect::<Vec<Piece>>().try_into().unwrap();
-    
+    let default_pieces: [Piece; 24] = ALL_DEFAULT_PIECES_COORDINATES
+        .iter()
+        .map(|coordinates| Piece {
+            block_1: Coordinates {
+                x: coordinates[0].0,
+                y: coordinates[0].1,
+                z: coordinates[0].2,
+            },
+            block_2: Coordinates {
+                x: coordinates[1].0,
+                y: coordinates[1].1,
+                z: coordinates[1].2,
+            },
+            block_3: Coordinates {
+                x: coordinates[2].0,
+                y: coordinates[2].1,
+                z: coordinates[2].2,
+            },
+            block_4: Coordinates {
+                x: coordinates[3].0,
+                y: coordinates[3].1,
+                z: coordinates[3].2,
+            },
+            block_5: Coordinates {
+                x: coordinates[4].0,
+                y: coordinates[4].1,
+                z: coordinates[4].2,
+            },
+        })
+        .collect::<Vec<Piece>>()
+        .try_into()
+        .unwrap();
+
     return default_pieces;
 }
 
 pub fn are_pieces_overlapping(piece_1: Piece, piece_2: Piece) -> bool {
-    let blocks = HashSet::from([
-        piece_1.block_1, piece_1.block_2, piece_1.block_3, piece_1.block_4, piece_1.block_5,
-        piece_2.block_1, piece_2.block_2, piece_2.block_3, piece_2.block_4, piece_2.block_5,
-    ]);
+    if piece_1.contains(&piece_2.block_1)
+        || piece_1.contains(&piece_2.block_2)
+        || piece_1.contains(&piece_2.block_3)
+        || piece_1.contains(&piece_2.block_4)
+        || piece_1.contains(&piece_2.block_5)
+    {
+        return true;
+    }
 
-    return blocks.len() < 10;
+    return false;
 }
